9d2e7c2994b304eab247f1ca5bd3c58b51af8433
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2026-06-08 14:51:37 -0400

    finished 4-16

--- 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
@@ -1,11 +1,18 @@
 function keydownAction(event) {
   // TODO: Complete keydown function
+  document.querySelector("#status").textContent = "KEYDOWN Event";
+  document.querySelector("#code").textContent = event.code;
+  document.querySelector("#key").textContent = event.key;
+
 }
 
 function keyupAction() {
   document.querySelector("#status").textContent = "KEYUP Event";
+  document.querySelector("#code").textContent = "";
+  document.querySelector("#key").textContent = "";
 }
 
 document.addEventListener("keyup", keyupAction);
+document.addEventListener("keydown", keydownAction);
 // TODO: Add Event Listener for "keydown" event
 
