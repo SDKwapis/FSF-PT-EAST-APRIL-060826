89aaa45af122a3d6b4c38d209d824a7c75dda3dc
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2026-06-11 19:59:12 -0400

    finished 4-22

--- 04-Web-APIs/01-Activities/22-Stu_Local-Storage/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/22-Stu_Local-Storage/Unsolved/assets/js/script.js
@@ -5,6 +5,7 @@ const msgDiv = document.querySelector('#msg');
 const userEmailSpan = document.querySelector('#user-email');
 const userPasswordSpan = document.querySelector('#user-password');
 
+
 renderLastRegistered();
 
 function displayMessage(type, message) {
@@ -13,10 +14,14 @@ function displayMessage(type, message) {
 }
 
 function renderLastRegistered() {
-  emailInput.textcontent = inputEmail
+let email = localStorage.getItem('email');
+let password = localStorage.getItem('password');
+
+userEmailSpan.textContent = email;
+userPasswordSpan.textContent = password;
+}
   
   // TODO: Retrieve the last email and password and render it to the page
-}
 
 signUpButton.addEventListener('click', function (event) {
   event.preventDefault();
@@ -29,8 +34,13 @@ signUpButton.addEventListener('click', function (event) {
   } else if (password === '') {
     displayMessage('error', 'Password cannot be blank');
   } else {
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
     displayMessage('success', 'Registered successfully');
+console.log(`button press: ${email}`);
+console.log(`button press: ${password}`);  
+let count = localStorage.getItem('email');
 
+console.log(count)
     // TODO: Save email and password to localStorage and render the last registered user
-  }
-});
+}renderLastRegistered()  });
