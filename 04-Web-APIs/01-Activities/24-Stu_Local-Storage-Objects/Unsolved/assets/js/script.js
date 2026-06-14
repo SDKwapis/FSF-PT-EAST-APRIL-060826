e18181316d8d81f18fe84ddd9136a44bc0154992
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2026-06-14 18:54:35 -0400

    finished the activities, on to the challenges

--- 04-Web-APIs/01-Activities/24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
@@ -3,11 +3,24 @@ const lastNameInput = document.querySelector('#last-name');
 const emailInput = document.querySelector('#email');
 const passwordInput = document.querySelector('#password');
 const signUpButton = document.querySelector('#sign-up');
+const stored = document.querySelector('#stored');
 
 signUpButton.addEventListener('click', function (event) {
   event.preventDefault();
 
   // TODO: Create user object from submission
-
+const user = {
+  first: firstNameInput.value,
+  last: lastNameInput.value, 
+  email: emailInput.value,
+  password: passwordInput.value,
+};
   // TODO: Set new submission to local storage
+localStorage.setItem('user', JSON.stringify(user));
+renderStored();
 });
+
+function renderStored() {
+  const lastStored = JSON.parse(localStorage.getItem('user'));
+  stored.textContent = lastStored
+};
